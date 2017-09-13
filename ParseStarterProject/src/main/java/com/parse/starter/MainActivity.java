70ba78a5463694174cdebf287a4661a174b7d6c2
sourabh kulkarni <skulkar6@stevens.edu>
2017-09-12 23:44:26 -0400

    LoginInToApp

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -10,6 +10,9 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
@@ -25,50 +28,36 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public  void signUp(View view){
+        EditText usernameEditText=(EditText)findViewById(R.id.UsernameEditText);
+        EditText passwordEditText=(EditText)findViewById(R.id.PasswordEdittext);
+
+        if(usernameEditText.getText().toString().matches("")||passwordEditText.getText().toString().matches("")){
+            Toast.makeText(MainActivity.this,"Enter username/password",Toast.LENGTH_LONG).show();
+        }else{
+            ParseUser user=new ParseUser();
+            user.setUsername(usernameEditText.getText().toString());
+            user.setPassword(passwordEditText.getText().toString());
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                   if(e==null){
+                       Log.i("Sign up", "Success");
+                   }else{
+                       Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                   }
+                }
+            });
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*
-
-
-        ParseUser.logInInBackground("sourabhuk254", "password", new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if(user!=null){
-                    Log.i("Login ", "Successful");
-
-                }else{
-                    Log.i("Login "," Incorrect password");
-                }
-            }
-        });
-        */
-        ParseUser.logOut();
-        if(ParseUser.getCurrentUser()!=null){
-            Log.i("Message", "User is already logged in "+ParseUser.getCurrentUser().getUsername());
-        }else{
-            Log.i("Message", " Not logged in");
-        }
-/*
-
-
-        ParseUser user = new ParseUser();
-        user.setUsername("sHreyas");
-        user.setPassword("password1");
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e== null){
-                    Log.i("signup","Succerr");
-
-                }else{
-                    Log.i("Signup", "Failed");
-                }
-            }
-        });
-*/
 
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
