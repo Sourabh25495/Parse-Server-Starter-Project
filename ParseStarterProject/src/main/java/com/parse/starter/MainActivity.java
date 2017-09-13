d7cd9c9f308e76f02eba045a8d3e70965fc0af73
sourabh kulkarni <skulkar6@stevens.edu>
2017-09-13 19:14:22 -0400

    Login/SignupInToApp

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -8,10 +8,13 @@
  */
 package com.parse.starter;
 import android.os.Bundle;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -26,8 +29,28 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView ChangeSignUpModeTextVieW;
+    Boolean signUpModeActive= true;
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.ChangeSignupModetextView) {
+            Log.d("App info", "Change signup mode");
+            Button signupButton =(Button)findViewById(R.id.signUpButton2);
+            if (signUpModeActive) {
+
+               signUpModeActive=false;
+                signupButton.setText("Login");
+                ChangeSignUpModeTextVieW .setText("Or, Signup");
+            }else{
+                signUpModeActive=true;
+                signupButton.setText("Sign up");
+                ChangeSignUpModeTextVieW .setText("Or, Login");
+            }
+        }
+    }
 
     public  void signUp(View view){
         EditText usernameEditText=(EditText)findViewById(R.id.UsernameEditText);
@@ -36,19 +59,32 @@ public class MainActivity extends AppCompatActivity {
         if(usernameEditText.getText().toString().matches("")||passwordEditText.getText().toString().matches("")){
             Toast.makeText(MainActivity.this,"Enter username/password",Toast.LENGTH_LONG).show();
         }else{
-            ParseUser user=new ParseUser();
-            user.setUsername(usernameEditText.getText().toString());
-            user.setPassword(passwordEditText.getText().toString());
-            user.signUpInBackground(new SignUpCallback() {
-                @Override
-                public void done(ParseException e) {
-                   if(e==null){
-                       Log.i("Sign up", "Success");
-                   }else{
-                       Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
-                   }
-                }
-            });
+            if(signUpModeActive) {
+                ParseUser user = new ParseUser();
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("Sign up", "Success");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }else {
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user!=null){
+                            Log.i("Login:", "Successful");
+                        }else{
+                            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
         }
 
     }
@@ -58,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ChangeSignUpModeTextVieW =(TextView) findViewById(R.id.ChangeSignupModetextView);
+        ChangeSignUpModeTextVieW.setOnClickListener(this);
 
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
